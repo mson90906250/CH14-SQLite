e0da9d8293e1d94d66005c3ef774eb80d3588ae0
mson90906250 <l90906250@gmail.com>
2019-01-23 10:28:13 +0800

    在M1405insert裡新增menu語法 修正一些警告

--- 1405_SQLite_DBHper_add/src/main/java/tw/tcnr01/m1405/M1405insert.java
+++ 1405_SQLite_DBHper_add/src/main/java/tw/tcnr01/m1405/M1405insert.java
@@ -1,4 +1,33 @@
 package tw.tcnr01.m1405;
 
-class M1405insert {
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+
+class M1405insert extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+
+
+
+    //------------------------menu-----------------------------------
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.m1405sub, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        int id = item.getItemId();
+        if (id == R.id.m_return) finish();
+        return true;
+    }
+
 }
